988de66848ebc8aefcd6c09cb020f67161ce1b34
Liliia Chuiko <chuiko1112@gmail.com>
2022-01-18 12:59:39 +0200

    added db connection

--- index.js
+++ index.js
@@ -1,64 +1,35 @@
 const schedule = require('node-schedule');
-const fetch = require("node-fetch");
-const $ = require('cheerio');
-const rp = require('request-promise');
+const { MongoClient } = require('mongodb');
 const { parse } = require('node-html-parser');
-
-// // schedule.scheduleJob('55 * * * * *',  async function(){
-// //   console.log('The answer to life, the universe, and everything!');
-// //   const res = await fetch('https://dribbble.com/shots/popular')
-// //   console.log(res)
-// // });
-
-// const func = async () => {
-//   console.log('The answer to life, the universe, and everything!');
-//   const html = await rp('https://dribbble.com/shots/popular')
-//   const root = parse(html);
-//   const items = root.querySelectorAll('.shot-thumbnail-container');
-//   const info = items.map((item, index) => {
-//     const name = item.querySelector('.display-name').childNodes[0]._rawText;
-//     const views = item.querySelector('.js-shot-views-count').childNodes[0]._rawText;
-//     const likes = item.querySelector('.js-shot-likes-count').childNodes[0]._rawText;
-//     if (index === 2) console.log({name})
-//     return {
-//       name, views, likes, position: index + 1
-//     }
-//   })
-//   console.log({info})
-// }
-
-// func()
-
-// var newestShots = [{
-  const { copyFileSync } = require('fs');
+require('dotenv').config();
 const puppeteer = require('puppeteer');
-  async function getTokoPedia(){
-      const browser = await puppeteer.launch({ headless: true, slowMo: 250, args: ['--no-sandbox', '--disable-setuid-sandbox']}); // for test disable the headlels mode,
-      const page = await browser.newPage();
 
-      // await page.setViewport({ width: 1000, height: 9926 });
-      await page.goto("https://dribbble.com/shots/popular",{waitUntil: 'networkidle2'});
-      await autoScroll(page);
-  
-      const html = await page.evaluate(() => document.querySelector('*').outerHTML);
-      const root = parse(html);
-      const items = root.querySelectorAll('.shot-thumbnail-container');
-      const info = items.map((item, index) => {
-        const name = item.querySelector('.display-name').childNodes[0]._rawText;
-        const views = item.querySelector('.js-shot-views-count');
-        const viewsCount = views ? views.childNodes[0]._rawText : undefined
-        const likes = item.querySelector('.js-shot-likes-count');
-        const likesCount = likes ? likes.childNodes[0]._rawText : undefined
-        // if (index === 2) console.log({name})
-        return {
-          name, viewsCount, likesCount, i: index+1
-        }
-      })
-      console.log({info})
+async function getTokoPedia(){
+    const browser = await puppeteer.launch({ headless: true, slowMo: 250, args: ['--no-sandbox', '--disable-setuid-sandbox']}); // for test disable the headlels mode,
+    const page = await browser.newPage();
+    await page.goto("https://dribbble.com/shots/popular",{waitUntil: 'networkidle2'});
+    await autoScroll(page);
 
-  
-      // browser.close()
-  } 
+    const html = await page.evaluate(() => document.querySelector('*').outerHTML);
+    const root = parse(html);
+    const items = root.querySelectorAll('.shot-thumbnail-container');
+    const info = items.map((item, index) => {
+      const company = item.querySelector('.display-name').childNodes[0]._rawText;
+      const views = item.querySelector('.js-shot-views-count');
+      const viewsCount = views ? views.childNodes[0]._rawText : undefined
+      const likes = item.querySelector('.js-shot-likes-count');
+      const likesCount = likes ? likes.childNodes[0]._rawText : undefined
+      const title = item.querySelector('.shot-title');
+      const titleProcessed = title ? title.childNodes[0]._rawText : 'boosted';
+      return {
+        company, viewsCount, likesCount, i: index+1, id: item._attrs['data-thumbnail-id'], title: titleProcessed
+      }
+    })
+    const time = Date.now();
+    
+    browser.close()
+    return { info, time }
+} 
 
   async function autoScroll(page){
     await page.evaluate(async () => {
@@ -78,6 +49,51 @@ const puppeteer = require('puppeteer');
         });
     });
 }
-  
-  
-  getTokoPedia();
+
+const putNewValuesToDatabase = async ({ info, time }) => {
+  try {
+    const client = new MongoClient(encodeURI(process.env.MONGO_URI), { useNewUrlParser: true , useUnifiedTopology: true });
+    await client.connect();
+    const database = client.db(process.env.MONGO_DATABASE_NAME);
+    const collection = database.collection('shots');
+    for (let j = 0; j < info.length; j++) {
+      const { viewsCount, likesCount, i, id, title, } = info[j];
+
+      const cursorFrom = await collection.find({ id });
+      const selectedDataFrom = await cursorFrom.toArray();
+      if (selectedDataFrom.length === 0) {
+        await collection.insertOne({ id, title, shots: [{ position: i, likesCount, viewsCount, time }]})
+      } else {
+        await collection.updateOne(
+          { id }, 
+          { $set: { 
+            shots: [...selectedDataFrom[0].shots, { position: i, likesCount, viewsCount, time }] 
+          }}
+        )
+        console.log({selectedDataFrom})
+      }
+      // console.log({ selectedDataFrom })
+    }
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+
+schedule.scheduleJob('55 * * * * *',  async function(){
+  const { time, info } = await getTokoPedia();
+
+  const regex = new RegExp('Halo Lab', 'gim');
+  const haloInfo  = info.filter(item => regex.test(item.company));
+  const haloInfoProcessed  = haloInfo.map(item => {
+    const viewsNumberK = item.viewsCount.split('k')[0];
+    const viewsNumber = viewsNumberK.length === item.viewsCount.length ? +viewsNumberK : +viewsNumberK * 1000
+    console.log({viewsNumber})
+    return {
+      ...item,
+      viewsCount: viewsNumber
+    }
+  });
+  if (haloInfo.length === 0) return
+  await putNewValuesToDatabase({ info: haloInfoProcessed, time})
+});
